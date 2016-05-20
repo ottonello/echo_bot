8dbb29b844a7dbe48757c9634ee7c8fcbb11adf8
Marcos Ottonello <marcos@ngti.nl>
2016-05-21 00:37:49 +0200

    generalizing bots

--- bots/echoBot.go
+++ bots/echoBot.go
@@ -9,7 +9,7 @@ func NewEchoBot(initialMessage string) *EchoBot {
 	return &EchoBot{initialMessage, make(map[string]bool)}
 }
 
-func (e EchoBot) message(from string, message string) (error, string) {
+func (e EchoBot) ReceivedMessage(from string, message string) (error, string) {
 	msg := message
 	if e.knownUsers[from] == false {
 		msg = e.initialMessage
